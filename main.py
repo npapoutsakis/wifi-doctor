edb04ff8ece667b13492c0da97394a2d7708f4aa
npapoutsakis <nikospapoutsak@gmail.com>
2025-03-20 12:58:44 +0200

    just added some files and comments to start

---
+++ main.py
@@ -0,0 +1,36 @@
+# WiFi Doctor - Networks 2025
+
+# Group 4: 
+#       - Nikolaos Papoutsakis  2019030206
+#       - Argyris Christakis    2019030078
+#       - Michalis Syrianos     2019030058
+#       - Swkratis Siganos      2019030097
+
+
+# Our main idea is to structure a simple .py file to run the 
+# use-cases described in the assignment's description.
+
+
+# First implemente the wifi doctor system, and then run the use-cases.
+
+
+# The parser function will read the .pcap files and return a dictionary of the data.(or json?)
+def parser(pcap_file):
+
+    dict = {"SSID": "TUC"}
+
+
+
+    return dict
+
+
+
+def main():
+
+    return
+
+
+
+
+if __name__ == "__main__":
+    main()
