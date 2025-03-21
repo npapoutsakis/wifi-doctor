8f7b0da206f025eb0cd2de67be306d0d8626150e
npapoutsakis <nikospapoutsak@gmail.com>
2025-03-21 19:42:17 +0200

    made a simple sniffer + added util files

--- main.py
+++ main.py
@@ -9,15 +9,36 @@
 
 # Our main idea is to structure a simple .py file to run the 
 # use-cases described in the assignment's description.
+import pyshark
 
 
-# First implemente the wifi doctor system, and then run the use-cases.
+# First implement the wifi doctor system, and then run the use-cases.
+def wifi_sniffer(interface='enp5s0', packet_count=10):
+
+    capture = pyshark.LiveCapture(interface=interface)
+    
+    
+    for i, packet in enumerate(capture.sniff_continuously(packet_count=int(packet_count))):
+        print(f"Packet #{i + 1}: {packet}")
+        print("=" * 50)
+
+    print(f"Capture complete. {packet_count} packets captured.")
+
+    return
+
 
 
 # The parser function will read the .pcap files and return a dictionary of the data.(or json?)
-def parser(pcap_file):
+def parser():
 
-    dict = {"SSID": "TUC"}
+    dict = {
+        "SSID": "TUC",
+        "BSSID": "00:00:00:00:00:00",
+        "Channel": "1",
+        "RSSI": "-50",
+        "Security": "WPA2",
+        "Beacon": "100",   
+        }
 
 
 
@@ -25,7 +46,14 @@ def parser(pcap_file):
 
 
 
+
+
+
 def main():
+
+    # Run the wifi sniffer
+    wifi_sniffer()
+
 
     return
 
