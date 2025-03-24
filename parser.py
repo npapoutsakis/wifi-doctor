6a08d4932ddf7365860ef68ecd87e4d2bf05d2d8
socsig2210-git <socsig2210@gmail.com>
2025-03-25 01:50:04 +0200

    parser wip, pyshark for parsing using wireshark docs, prob inmemory list, parse

--- parser.py
+++ parser.py
@@ -1,3 +1,122 @@
 """
-    Pcap Parsing Tool
+Pcap Parsing Tool
 """
+
+from scapy.utils import rdpcap
+import pyshark
+import scapy.layers.dot11 as dot11
+import wlan_packet
+import json
+import time
+
+PCAP_FILE = "pcaps/HowIWiFi_PCAP.pcap"
+
+"""Pyshark"""
+
+
+# TODO: create two different captures, one for beacone fr(1.1) one for data fr(1.2)
+start_time = time.time()
+capture = pyshark.FileCapture(PCAP_FILE, use_json=True)
+for packet in capture:
+    radiotap = packet.radiotap  # Radiotap header
+    radio = packet.wlan_radio  # 802.11 radio
+    wlan = packet.wlan  # 802.11 wlan
+
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
+
+# packets = rdpcap(PCAP_FILE, count=1)
+# for packet in packets:
+#     print(packet)
+
+# wlan_packets = []
+# for packet in packets:
+#     if packet.haslayer(dot11.Dot11):
+#         ssid = packet.info.decode() if packet.haslayer(dot11.Dot11Beacon) else "Unknown"
+#         bssid = packet.addr2 if packet.addr2 else "00:00:00:00:00:00"
+#         transmitter_mac = packet.addr2 if packet.addr2 else "00:00:00:00:00:00"
+#         receiver_mac = packet.addr1 if packet.addr1 else "00:00:00:00:00:00"
+#         wlan_type = packet.type if hasattr(packet, "type") else 0
+#         wlan_subtype = packet.subtype if hasattr(packet, "subtype") else 0
+#         phy_type = "Unknown"
+#         mcs_index = 0
+#         bandwidth = 20
+#         short_gi = False
+#         data_rate = 0.0
+#         channel = 0
+#         frequency = 0
+#         rssi = packet.dBm_AntSignal if hasattr(packet, "dBm_AntSignal") else -100
+#         timestamp = packet.time if hasattr(packet, "time") else 0.0
+#         snr = None
+#         spatial_streams = None
+
+#         wlan_packet_obj = wlan_packet(
+#             ssid=ssid,
+#             bssid=bssid,
+#             transmitter_mac=transmitter_mac,
+#             receiver_mac=receiver_mac,
+#             wlan_type=wlan_type,
+#             wlan_subtype=wlan_subtype,
+#             phy_type=phy_type,
+#             mcs_index=mcs_index,
+#             bandwidth=bandwidth,
+#             short_gi=short_gi,
+#             data_rate=data_rate,
+#             channel=channel,
+#             frequency=frequency,
+#             rssi=rssi,
+#             timestamp=timestamp,
+#             snr=snr,
+#             spatial_streams=spatial_streams,
+#         )
+#         wlan_packets.append(wlan_packet_obj)
+
+
+beacon = association_req = association_res = auth = deauth = total = total_wlan = 0
+
+
+# def save_packets_to_json(packet: "wlan_packet", file: str):
+#     """Append packet to json file"""
+#     with open(file, "a") as f:
+#         json.dump(packet, f, indent=2)
+
+
+# def get_layers(packet):
+#     layers = []
+#     while packet:
+#         layer = packet.__class__.__name__
+#         layers.append(layer)
+#         packet = packet.payload
+#     return layers
+
+
+# 1.2. (wlan.fc.type_subtype == 0x0020 || wlan.fc.type_subtype == 0x0028) && wlan.addr == 2C:F8:9B:DD:06:A0 && wlan.addr == 00:20:A6:FC:B0:36
+
+
+# for packet in packets:
+#     if not packet.haslayer(dot11.Dot11Beacon):
+#         layers = get_layers(packet)
+#         print(layers)
+# for packet in packets:
+#     total += 1
+#     layers = get_layers(packet)
+#     if packet.haslayer(dot11.Dot11):
+#         total_wlan += 1
+#         if packet.haslayer(dot11.Dot11Beacon):
+#             beacon += 1
+#         elif packet.haslayer(dot11.Dot11AssoReq):
+#             association_req += 1
+#         elif packet.haslayer(dot11.Dot11AssoResp):
+#             association_res += 1
+#         elif packet.haslayer(dot11.Dot11Auth):
+#             auth += 1
+#         elif packet.haslayer(dot11.Dot11Deauth):
+#             deauth += 1
+# print(f"Total: {total}")
+# print(f"Total WLAN: {total_wlan}")
+# print(f"Beacon: {beacon}")
+# print(f"Association Request: {association_req}")
+# print(f"Association Response: {association_res}")
+# print(f"Authentication: {auth}")
+# print(f"Deauthentication: {deauth}")
---
+++ wlan_packet.py
@@ -0,0 +1,125 @@
+import json
+from typing import Optional, List, Dict
+import random
+
+
+# TODO: In memory List with all wlan packets data, or save to json file?
+class wlan_packet:
+    def __init__(
+        self,
+        ssid: str,
+        bssid: str,
+        transmitter_mac: str,
+        receiver_mac: str,
+        wlan_type: int,
+        wlan_subtype: int,
+        phy_type: str,
+        mcs_index: int,
+        bandwidth: int,
+        short_gi: bool,
+        data_rate: float,
+        channel: int,
+        frequency: int,
+        rssi: int,
+        timestamp: float,
+        snr: int | None = None,
+        spatial_streams: int | None = None,
+    ):
+        self.ssid = ssid
+        self.bssid = bssid
+        self.transmitter_mac = transmitter_mac
+        self.receiver_mac = receiver_mac
+        self.wlan_type = wlan_type
+        self.wlan_subtype = wlan_subtype
+        self.phy_type = phy_type
+        self.mcs_index = mcs_index
+        self.bandwidth = bandwidth
+        self.short_gi = short_gi
+        self.data_rate = data_rate
+        self.channel = channel
+        self.frequency = frequency
+        self.rssi = rssi
+        self.timestamp = timestamp
+        self.snr = snr
+        self.spatial_streams = spatial_streams
+
+    # @classmethod
+    # def read_wlan_packet(cls, data: Dict) -> "wlan_packet":
+    #     """Create a wlan packet from json deserialization."""
+    #     return cls(
+    #         ssid=data["ssid"],
+    #         bssid=data["bssid"],
+    #         transmitter_mac=data["transmitter_mac"],
+    #         receiver_mac=data["receiver_mac"],
+    #         wlan_type=data["wlan_type"],
+    #         wlan_subtype=data["wlan_subtype"],
+    #         phy_type=data["phy_type"],
+    #         mcs_index=data["mcs_index"],
+    #         bandwidth=data["bandwidth"],
+    #         short_gi=data["short_gi"],
+    #         data_rate=data["data_rate"],
+    #         channel=data["channel"],
+    #         frequency=data["frequency"],
+    #         rssi=data["rssi"],
+    #         snr=data["snr"],
+    #         spatial_streams=data["spatial_streams"],
+    #     )
+
+    def __repr__(self):
+        return f"wlan packet(ssid={self.ssid}, signal={self.rssi} dBm)"
+
+
+# def save_packets_to_json(packet: "wlan_packet", file: str):
+#     """Append packet to json file"""
+#     with open(file, "a") as f:
+#         json.dump(packet, f, indent=2)
+
+
+# def load_packets_from_json(file: str) -> List[wlan_packet]:
+#     """Load packet from a json file."""
+#     with open(file, "r") as f:
+#         data = json.load(f)
+#     return [wlan_packet.from_dict(p) for p in data]
+
+
+def random_wlan_packet():
+    return wlan_packet(
+        ssid=f"SSID_{random.randint(1, 100)}",
+        bssid=f"{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}",
+        transmitter_mac=f"{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}",
+        receiver_mac=f"{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}",
+        wlan_type=random.randint(0, 255),
+        wlan_subtype=random.randint(0, 255),
+        phy_type=random.choice(["HT", "VHT", "HE"]),
+        mcs_index=random.randint(0, 31),
+        bandwidth=random.choice([20, 40, 80, 160]),
+        short_gi=random.choice([True, False]),
+        data_rate=random.uniform(1.0, 1000.0),
+        channel=random.randint(1, 14),
+        frequency=random.randint(2412, 2484),
+        timestamp=27.0002,
+        rssi=random.randint(-100, 0),
+        snr=random.randint(0, 40),
+        spatial_streams=random.randint(1, 8),
+    )
+
+
+def main():
+    # Create 3 random wlan_packet objects
+    packets = [random_wlan_packet() for _ in range(3)]
+    packets[0].spatial_streams = None
+
+    # Save the packets to a JSON file
+    # with open("packets.json", "a") as f:
+    #     json.dump([packet.__dict__ for packet in packets], f, indent=2)
+
+    # Load the packets from the JSON file
+    # packets = load_packets_from_json("packets.json")
+
+    # Print the packets
+    for packet in packets:
+        print(packet)
+
+
+# main()
+## test
